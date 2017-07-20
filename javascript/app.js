90c13acc0c3876bfd001dada9b4d776bbfc5a583
Derek Cassel <derekdcassel@gmail.com>
2017-07-20 13:02:38 -0600

    winner

--- javascript/app.js
+++ javascript/app.js
@@ -143,6 +143,7 @@ var game = {
     panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
     panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
     panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  
   }
 };
 
